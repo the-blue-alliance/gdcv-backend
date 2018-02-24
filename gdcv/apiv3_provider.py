6a8da3feb4a2fbf6fc1e39bdef8aa6d51701fce6
Phil Lopreiato <plnyyanks@gmail.com>
2018-02-24 08:40:06 -0500

    Add apiv3 method to fetch event matches

--- gdcv/apiv3_provider.py
+++ gdcv/apiv3_provider.py
@@ -4,6 +4,7 @@ from metadata_provider import MetadataProvider
 
 class ApiV3Provider(object):
 
+    AUTH_HEADER = 'X-TBA-Auth-Key'
     BASE_URL = 'https://www.thebluealliance.com/api/v3/{}'
 
     def __init__(self, metadata: MetadataProvider):
@@ -12,6 +13,16 @@ class ApiV3Provider(object):
     def _get_auth_key(self):
         return self.metadata.get('apiv3_auth_key')
 
+    def fetch_event_matches(self, event_key):
+        auth_key = self._get_auth_key()
+        if not auth_key:
+            logging.error("No apiv3 auth key found")
+            return None
+        url = self.BASE_URL.format('event/{}/matches'.format(event_key))
+        logging.debug("Making apiv3 request: {}".format(url))
+        r = requests.get(url, headers={self.AUTH_HEADER: auth_key})
+        return r.json()
+
     def fetch_match_details(self, match_key):
         auth_key = self._get_auth_key()
         if not auth_key:
@@ -19,5 +30,5 @@ class ApiV3Provider(object):
             return None
         url = self.BASE_URL.format('match/{}'.format(match_key))
         logging.debug("Making apiv3 request: {}".format(url))
-        r = requests.get(url, headers={'X-TBA-Auth-Key': auth_key})
+        r = requests.get(url, headers={self.AUTH_HEADER: auth_key})
         return r.json()
