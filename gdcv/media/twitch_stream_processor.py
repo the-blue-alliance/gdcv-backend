26ce1475c4454267f152d302cd77494c9c96a0a3
Eugene Fang <fang.eugene@gmail.com>
2018-03-02 09:46:04 -0500

    Update stream processor (#1)

--- gdcv/media/twitch_stream_processor.py
+++ gdcv/media/twitch_stream_processor.py
@@ -27,7 +27,7 @@ class TwitchStreamProcessor(object):
         logging.info("Loading streams for {}".format(stream_url))
         streams = streamlink.streams(stream_url)
         stream = None
-        for quality in ['720p', '1080p', 'best', 'source', '480p', '360p', '160p']:  # Order of decreasing priority
+        for quality in ['720p', '720p60']:  # Order of decreasing priority
             if quality in streams:
                 stream = streams[quality]
                 break
@@ -42,22 +42,26 @@ class TwitchStreamProcessor(object):
     def process_stream(self, event_key: str, stream: Stream, frame_callback: Callable):
         stop = False
         try:
-            pipe = sp.Popen(['ffmpeg', "-i", stream.url,
-                      "-loglevel", "quiet", # no text output
-                      "-an",   # disable audio
-                      "-f", "image2pipe",
-                      "-pix_fmt", "bgr24",
-                      "-vcodec", "rawvideo", "-"],
-                      stdin = sp.PIPE, stdout = sp.PIPE)
+            pipe = sp.Popen([
+                'ffmpeg',
+                '-loglevel', 'warning',
+                '-re',
+                '-timeout', '5',
+                '-i', stream.url,
+                '-an',   # disable audio
+                '-f', 'image2pipe',
+                '-vf', 'fps={}'.format(FPS),
+                '-pix_fmt', 'bgr24',
+                '-vcodec', 'rawvideo', '-'],
+                stdout=sp.PIPE, bufsize=8**10)
 
-            image_queue = Queue()
-            def image_worker():
+            data_queue = Queue()
+            def worker():
                 while not stop:
-                    raw_image = pipe.stdout.read(1280*720*3)
-                    image_queue.put(raw_image)
-                    time.sleep(1.0 / 60.0)  # Shorter than 1/30 be safe
-
-            w = threading.Thread(target=image_worker)
+                    data = pipe.stdout.read(720 * 1280 * 3)
+                    if data:
+                        data_queue.put(data)
+            w = threading.Thread(target=worker)
             w.start()
 
             start_process_time = time.time()
@@ -65,42 +69,39 @@ class TwitchStreamProcessor(object):
             while True:
                 cur_time = time.time()
                 if cur_time >= last_process_time + 1.0 / self.FPS:
-                    logging.info("Queue length: {}".format(image_queue.qsize()))
                     error = cur_time - last_process_time
                     last_process_time = cur_time
 
-                    num_frames = int(error * 30) + 1
-                    logging.info("Skipping {} Frames".format(num_frames))
-                    raw_image = None
-                    for _ in range(num_frames):
-                        if image_queue.qsize() > 0:
-                            raw_image = image_queue.get()
-                    if not raw_image:
-                        logging.warning("Unable to read image from stream queue")
-                        continue
-                    frame_start = time.time()
-                    image = np.fromstring(raw_image, dtype='uint8').reshape((720,1280,3))
-                    try:
-                        match_state = frame_callback(event_key, image)
-                    except Exception:
-                        logging.error("Exception processing frame")
-                        match_state = None
-                    runtime = time.time() - start_process_time
-                    frame_latency = time.time() - frame_start
-                    logging.info("Frame processed in {} seconds".format(frame_latency))
-                    timeout = runtime > self.TIMEOUT_SEC \
-                        and match_state != 'auto' \
-                        and match_state != 'teleop'
+                    qsize = data_queue.qsize()
+                    logging.info("Queue length: {}".format(qsize))
+                    if qsize > 0:
+                        data = data_queue.get()
+                        frame_start = time.time()
+                        image = np.fromstring(data, dtype='uint8').reshape((720, 1280, 3)))
+                        if image is not None:
+                            try:
+                                match_state = frame_callback(event_key, image)
+                            except Exception:
+                                logging.error("Exception processing frame")
+                                match_state = None
 
-                    # Stop processing after one match or if we go long enough
-                    # without seeing another match start
-                    if match_state == 'post_match' or timeout:
-                        logging.info(
-                            "Stopping processing. Running for {} seconds. Current state is {}".
-                            format(runtime, match_state))
-                        stop = True
-                        w.join()
-                        return 'nack'
+                        runtime = time.time() - start_process_time
+                        frame_latency = time.time() - frame_start
+                        logging.info("Frame processed in {} seconds".format(frame_latency))
+
+                        timeout = runtime > self.TIMEOUT_SEC \
+                            and match_state != 'auto' \
+                            and match_state != 'teleop'
+
+                        # Stop processing after one match or if we go long enough
+                        # without seeing another match start
+                        if match_state == 'post_match' or timeout:
+                            logging.info(
+                                "Stopping processing. Running for {} seconds. Current state is {}".
+                                format(runtime, match_state))
+                            stop = True
+                            w.join()
+                            return 'nack'
         except:
             logging.exception("Error processing stream")
             stop = True
